ef52ed56658cbdb79ac6adf6824fb36602bd009d
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-05-26 17:13:37 -0500

    Final Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Generating Data
+import { faker } from "@faker-js/faker";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Other resources
+import "./style/App.css";
+
+// React lesson files
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <h4>Warning!</h4>
+                <div>Are you sure you want to do this?</div>
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 6:00PM"
+                    avatarSrc={faker.image.avatar()}
+                    content="Nice blog post!"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Amy"
+                    timeAgo="Yesterday at 6:00PM"
+                    avatarSrc={faker.image.avatar()}
+                    content="Really nice!"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Dan"
+                    timeAgo="Today at 7:00PM"
+                    avatarSrc={faker.image.avatar()}
+                    content="Absolutely hated it."
+                />
+            </ApprovalCard>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
