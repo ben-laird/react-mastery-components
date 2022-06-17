75fa487e1a8ba546403ec45a1376b3863b50abb5
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-06-17 17:04:57 -0500

    Switch to TSX

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 // Generating Data
 import { faker } from "@faker-js/faker";
