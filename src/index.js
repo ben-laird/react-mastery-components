93d1869703ca58bf3a8145df37e30dce2c80d711
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-06-17 16:37:57 -0500

    Format via Prettier

--- src/index.js
+++ src/index.js
@@ -12,39 +12,39 @@ import CommentDetail from "./CommentDetail";
 import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <ApprovalCard>
-                <h4>Warning!</h4>
-                <div>Are you sure you want to do this?</div>
-            </ApprovalCard>
-            
-            <ApprovalCard>
-                <CommentDetail
-                    author="Sam"
-                    timeAgo="Today at 6:00PM"
-                    avatarSrc={faker.image.avatar()}
-                    content="Nice blog post!"
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail
-                    author="Amy"
-                    timeAgo="Yesterday at 6:00PM"
-                    avatarSrc={faker.image.avatar()}
-                    content="Really nice!"
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail
-                    author="Dan"
-                    timeAgo="Today at 7:00PM"
-                    avatarSrc={faker.image.avatar()}
-                    content="Absolutely hated it."
-                />
-            </ApprovalCard>
-        </div>
-    );
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <h4>Warning!</h4>
+        <div>Are you sure you want to do this?</div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 6:00PM"
+          avatarSrc={faker.image.avatar()}
+          content="Nice blog post!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Amy"
+          timeAgo="Yesterday at 6:00PM"
+          avatarSrc={faker.image.avatar()}
+          content="Really nice!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Dan"
+          timeAgo="Today at 7:00PM"
+          avatarSrc={faker.image.avatar()}
+          content="Absolutely hated it."
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
